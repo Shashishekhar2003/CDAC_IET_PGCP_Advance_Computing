70fd0efed1e948a2009b2eb9c4aff3a0685ead52
Shashishekhar2003 <shashibache2003@gmail.com>
2026-04-08 10:59:11 +0530

    Added Program

---
+++ Core Java/Day_02/Day_02_Person/src/com/demo/TestPerson.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Person p1=new Person("Shashi","264");
+			System.out.println(p1);
+			
+			Person p2=new Person("Akkshay","55");
+			System.out.println(p2);
+				
+			Person p3=new Person();
+			p3.setPname("Vivek");
+			p3.setMob("563");
+			System.out.println(p3);
+
+		
+
+
+	}
+
+}
