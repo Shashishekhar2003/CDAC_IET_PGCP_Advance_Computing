fea2875bb7020c1e893fd62f9fa9893dbdb2e965
Shashishekhar2003 <shashibache2003@gmail.com>
2026-08-21 17:14:12 +0530

    Added Program

---
+++ MS .NET/CSharpDemos26/09Demo_OOP/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _09Demo_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDemo obj = new MyClass();
+            //obj.Foo();
+            obj.Log();
+
+        }
+    }
+    public interface IDemo
+    {
+        public static int id;
+        void Foo();
+        protected void Log()
+        {
+            Console.WriteLine("Log method from IDemo interface");
+
+        }
+    }
+
+    public class MyClass : IDemo
+    {
+        public void Foo()
+        {
+            Console.WriteLine("Foo method from myclass");
+        }
+        public void Log()
+        {
+            Console.WriteLine("Log method from myclass");
+        }
+    }
+}
